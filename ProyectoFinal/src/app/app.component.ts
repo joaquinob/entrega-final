be86af034adfe9e156ce4219eeaf420fcf437ce2
bilyana-ancheva <bilyana.ancheva@bootcamp-upgrade.com>
2024-07-21 18:03:57 +0200

    usuario UI commit falta importar el componente usuario al app.module.ts

--- ProyectoFinal/src/app/app.component.ts
+++ ProyectoFinal/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UsuarioComponent } from "./components/pages/perfil/usuario/usuario.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UsuarioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
