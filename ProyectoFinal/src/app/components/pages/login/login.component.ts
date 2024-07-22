d4bbf4c0dfc0b9086e357395070399167edd64de
Joaquin Ortega <joaquin.blanco@bootcamp-upgrade.com>
2024-07-22 19:19:36 +0200

    login html sin fallo

--- ProyectoFinal/src/app/components/pages/login/login.component.ts
+++ ProyectoFinal/src/app/components/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Form, FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Form, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
 
@@ -12,11 +12,11 @@ import { AuthService } from '../../../services/auth.service';
 })
 export class LoginComponent {
   password: string = 'password';
-  form!: Form;
+  form!: FormGroup;
 
   constructor(private builder: FormBuilder, private authService: AuthService, private router: Router) {
     this.form = builder.group({
-      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'username': new FormControl(null, [Validators.required, Validators.email]),
       'password': new FormControl(null, [Validators.required])
     })
   }
