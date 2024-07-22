280ab5b741921d0920b15aa40d88e2cd9017010e
Joaquin Ortega <joaquin.blanco@bootcamp-upgrade.com>
2024-07-22 19:11:10 +0200

    login

--- ProyectoFinal/src/app/components/pages/login/login.component.ts
+++ ProyectoFinal/src/app/components/pages/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { Form, FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  password: string = 'password';
+  form!: Form;
 
+  constructor(private builder: FormBuilder, private authService: AuthService, private router: Router) {
+    this.form = builder.group({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required])
+    })
+  }
+
+  login() {
+    const email: string = this.form
+  }
 }
