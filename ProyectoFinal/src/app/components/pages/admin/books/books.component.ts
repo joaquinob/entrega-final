8a7609d14e95cff3153673db97450911e82104f9
Joaquin Ortega <joaquin.blanco@bootcamp-upgrade.com>
2024-07-30 17:36:34 +0200

    update

--- ProyectoFinal/src/app/components/pages/admin/books/books.component.ts
+++ ProyectoFinal/src/app/components/pages/admin/books/books.component.ts
@@ -46,8 +46,8 @@ export class BooksComponent implements OnInit {
         this.bookService.deleteBook(bookId).subscribe({
           next: () => {
             Swal.fire({
-              title: "¡Reserva eliminada!",
-              text: "Tu reserva ha sido eliminada correctamente",
+              title: "¡Reseña eliminada!",
+              text: "La reseña ha sido eliminada correctamente",
               icon: "success",
               showConfirmButton: false,
               timer: 2000
